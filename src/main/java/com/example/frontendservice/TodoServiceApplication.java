9468cf8bcffae26c70f48f9a957becb06feb9622
Shaurya Jaiswal <shaurya.jaiswal@clover.com>
2023-04-23 20:51:24 +0530

    Changed the name of Service

--- src/main/java/com/example/frontendservice/TodoServiceApplication.java
+++ src/main/java/com/example/frontendservice/TodoServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FrontEndServiceApplication {
+public class TodoServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FrontEndServiceApplication.class, args);
+        SpringApplication.run(TodoServiceApplication.class, args);
     }
 
 }
